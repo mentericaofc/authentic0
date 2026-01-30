4bfe8c7be929a3cd2264647489309559e3173062
mentericaofc <ericpinho987@gmail.com>
2026-01-30 01:51:00 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const checkoutLink = "https://pay.cakto.com.br/3bdvaxo_747510";
+
+document.getElementById("checkoutBtn").onclick = () => {
+  window.location.href = checkoutLink;
+};
+
+document.getElementById("checkoutBtn2").onclick = () => {
+  window.location.href = checkoutLink;
+};
